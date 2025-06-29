3290b933166947e245589b2da114845ad002dee9
XircTwerk <ddanielcohen2010@gmail.com>
2025-06-29 01:53:55 -0700

    Fixed Forge

--- forge/src/main/java/com/xirc/nichirin/forge/BreathOfNichirinForge.java
+++ forge/src/main/java/com/xirc/nichirin/forge/BreathOfNichirinForge.java
@@ -4,15 +4,15 @@ import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-import com.xirc.nichirin.ExampleMod;
+import com.xirc.nichirin.BreathOfNichirin;
 
-@Mod(ExampleMod.MOD_ID)
-public final class ExampleModForge {
-    public ExampleModForge() {
+@Mod(BreathOfNichirin.MOD_ID)
+public final class BreathOfNichirinForge {
+    public BreathOfNichirinForge() {
         // Submit our event bus to let Architectury API register our content on the right time.
-        EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        EventBuses.registerModEventBus(BreathOfNichirin.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 
         // Run our common setup.
-        ExampleMod.init();
+        BreathOfNichirin.init();
     }
 }
