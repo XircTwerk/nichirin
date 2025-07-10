7b63c4fdb8b793de87848aad9ca777cf7442095f
XircTwerk <ddanielcohen2010@gmail.com>
2025-07-10 13:15:49 -0700

    removed logging from breathing handler

--- common/src/main/java/com/xirc/nichirin/common/event/BreathingEventHandler.java
+++ common/src/main/java/com/xirc/nichirin/common/event/BreathingEventHandler.java
@@ -24,25 +24,16 @@ public class BreathingEventHandler {
         // Initial sync when player joins
         PlayerEvent.PLAYER_JOIN.register(player -> {
             if (!player.level().isClientSide) {
-                System.out.println("=== PLAYER JOIN - INITIALIZING BREATHING ===");
-                System.out.println("Player: " + player.getName().getString());
-
                 // Force initialize breathing if needed
                 float currentBreath = BreathingManager.getBreath(player);
-                float maxBreath = BreathingManager.getMaxBreath(player);
-
-                System.out.println("Initial breath: " + currentBreath + "/" + maxBreath);
 
                 // If breath is 0, restore it to full
                 if (currentBreath <= 0) {
-                    System.out.println("Breath was 0, restoring to full");
                     BreathingManager.restoreFull(player);
                 }
 
                 // Force sync
                 BreathingManager.forceSyncToClient(player);
-
-                System.out.println("After init - breath: " + BreathingManager.getBreath(player) + "/" + BreathingManager.getMaxBreath(player));
             }
         });
 
@@ -54,7 +45,6 @@ public class BreathingEventHandler {
         // Restore full breath on respawn
         PlayerEvent.PLAYER_RESPAWN.register((newPlayer, conqueredEnd) -> {
             if (!conqueredEnd) {
-                System.out.println("=== PLAYER RESPAWN - RESTORING BREATHING ===");
                 BreathingManager.restoreFull(newPlayer);
 
                 // Force sync after respawn
