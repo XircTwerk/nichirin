71da7459f11b02c0a4424a40ad773ff58156c58f
XircTwerk <ddanielcohen2010@gmail.com>
2025-07-30 17:06:19 -0700

    more bento box cleaning up

--- common/src/main/java/com/xirc/nichirin/common/item/tool/BentoBoxItem.java
+++ common/src/main/java/com/xirc/nichirin/common/item/tool/BentoBoxItem.java
@@ -6,20 +6,18 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.Container;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.ChestMenu;
-import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.MenuProvider;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.jetbrains.annotations.Nullable;
@@ -39,47 +37,143 @@ public class BentoBoxItem extends Item {
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
 
+        // Prevent opening when using off-hand
+        if (hand == InteractionHand.OFF_HAND) {
+            return InteractionResultHolder.fail(stack);
+        }
+
+        // Prevent opening if player already has a container menu open (not their inventory)
+        if (player.containerMenu != player.inventoryMenu) {
+            return InteractionResultHolder.fail(stack);
+        }
+
         LOGGER.info("BentoBox use() - Hand: {}, Player: {}", hand, player.getName().getString());
 
         if (level.isClientSide) {
             return InteractionResultHolder.success(stack);
         }
 
-        player.openMenu(new MenuProvider() {
-            @Override
-            public Component getDisplayName() {
-                return Component.literal("Bento Box");
+        // Find the slot index of the bento box being opened
+        int bentoBoxSlotIndex = -1;
+        for (int i = 0; i < player.getInventory().items.size(); i++) {
+            if (player.getInventory().items.get(i) == stack) {
+                bentoBoxSlotIndex = i;
+                break;
             }
+        }
 
-            @Override
-            public AbstractContainerMenu createMenu(int syncId, Inventory playerInventory, Player player) {
-                SimpleContainer inventory = new SimpleContainer(BENTO_SIZE);
-                loadItemsFromNbt(stack, inventory);
-
-                ChestMenu handler = new ChestMenu(MenuType.GENERIC_9x1, syncId, playerInventory, inventory, 1) {
-                    @Override
-                    public void removed(Player player) {
-                        super.removed(player);
-                        saveItemsToNbt(stack, inventory);
-                        LOGGER.info("Saved bento box for player: {}", player.getName().getString());
-                    }
-                };
-
-                for (int i = 0; i < BENTO_SIZE; i++) {
-                    Slot oldSlot = handler.slots.get(i);
-                    Slot newSlot = new FoodOnlySlot(inventory, i, oldSlot.x, oldSlot.y);
-                    handler.slots.set(i, newSlot);
-                }
-
-                LOGGER.info("Created bento box menu with {} slots", BENTO_SIZE);
-                return handler;
-            }
-        });
-
+        player.openMenu(new BentoBoxMenuProvider(stack, bentoBoxSlotIndex));
         return InteractionResultHolder.success(stack);
     }
 
-    private void loadItemsFromNbt(ItemStack bentoBox, SimpleContainer inventory) {
+    private static class BentoBoxMenuProvider implements MenuProvider {
+        private final ItemStack bentoBoxStack;
+        private final int bentoBoxSlotIndex;
+
+        public BentoBoxMenuProvider(ItemStack stack, int slotIndex) {
+            this.bentoBoxStack = stack;
+            this.bentoBoxSlotIndex = slotIndex;
+        }
+
+        @Override
+        public Component getDisplayName() {
+            return Component.literal("Bento Box");
+        }
+
+        @Override
+        public AbstractContainerMenu createMenu(int syncId, Inventory playerInventory, Player player) {
+            SimpleContainer inventory = new SimpleContainer(BENTO_SIZE);
+            loadItemsFromNbt(bentoBoxStack, inventory);
+
+            return new BentoBoxMenu(syncId, playerInventory, inventory, bentoBoxSlotIndex);
+        }
+    }
+
+    private static class BentoBoxMenu extends ChestMenu {
+        private final int bentoBoxSlotIndex;
+
+        public BentoBoxMenu(int syncId, Inventory playerInventory, SimpleContainer inventory, int bentoBoxSlotIndex) {
+            super(MenuType.GENERIC_9x1, syncId, playerInventory, inventory, 1);
+            this.bentoBoxSlotIndex = bentoBoxSlotIndex;
+
+            // Replace the container slots with food-only slots
+            for (int i = 0; i < BENTO_SIZE; i++) {
+                Slot oldSlot = this.slots.get(i);
+                Slot newSlot = new FoodOnlySlot(inventory, i, oldSlot.x, oldSlot.y);
+                this.slots.set(i, newSlot);
+            }
+
+            // Replace the bento box slot with a disabled slot
+            // ChestMenu creates slots in this order: container slots, then player inventory, then hotbar
+            // For GENERIC_9x1: slots 0-8 are container, 9-35 are player inventory, 36-44 are hotbar
+            replaceBentoBoxSlotWithDisabled(playerInventory);
+
+            LOGGER.info("Created bento box menu with {} slots", BENTO_SIZE);
+        }
+
+        private void replaceBentoBoxSlotWithDisabled(Inventory playerInventory) {
+            // Find the slot in the menu that corresponds to the bento box
+            for (int i = BENTO_SIZE; i < this.slots.size(); i++) {
+                Slot slot = this.slots.get(i);
+                if (slot.container == playerInventory && slot.getContainerSlot() == bentoBoxSlotIndex) {
+                    // Replace with disabled slot
+                    DisabledSlot disabledSlot = new DisabledSlot(playerInventory, bentoBoxSlotIndex, slot.x, slot.y);
+                    this.slots.set(i, disabledSlot);
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void clicked(int slotId, int button, ClickType clickType, Player player) {
+            // Prevent any interaction with the disabled bento box slot
+            if (slotId >= 0 && slotId < this.slots.size()) {
+                Slot slot = this.slots.get(slotId);
+                if (slot instanceof DisabledSlot) {
+                    // Don't call super - completely prevent the interaction
+                    return;
+                }
+            }
+
+            // For all other slots, proceed normally
+            super.clicked(slotId, button, clickType, player);
+        }
+
+        @Override
+        public void removed(Player player) {
+            super.removed(player);
+            // Save items back to the bento box in player's inventory
+            ItemStack bentoBoxStack = player.getInventory().items.get(bentoBoxSlotIndex);
+            if (bentoBoxStack.getItem() instanceof BentoBoxItem) {
+                saveItemsToNbt(bentoBoxStack, (SimpleContainer) this.getContainer());
+                LOGGER.info("Saved bento box for player: {}", player.getName().getString());
+            }
+        }
+
+        @Override
+        public boolean stillValid(Player player) {
+            // Check if the bento box is still in the correct slot
+            if (bentoBoxSlotIndex >= 0 && bentoBoxSlotIndex < player.getInventory().items.size()) {
+                ItemStack stack = player.getInventory().items.get(bentoBoxSlotIndex);
+                return stack.getItem() instanceof BentoBoxItem;
+            }
+            return false;
+        }
+    }
+
+    // Disabled slot that prevents pickup
+    private static class DisabledSlot extends Slot {
+        public DisabledSlot(Container container, int index, int xPosition, int yPosition) {
+            super(container, index, xPosition, yPosition);
+        }
+
+        @Override
+        public boolean mayPickup(Player player) {
+            return false;
+        }
+    }
+
+    private static void loadItemsFromNbt(ItemStack bentoBox, SimpleContainer inventory) {
         if (!bentoBox.hasTag() || !bentoBox.getTag().contains(ITEMS_TAG)) {
             return;
         }
@@ -96,7 +190,7 @@ public class BentoBoxItem extends Item {
         }
     }
 
-    private void saveItemsToNbt(ItemStack bentoBox, SimpleContainer inventory) {
+    private static void saveItemsToNbt(ItemStack bentoBox, SimpleContainer inventory) {
         ListTag nbtList = new ListTag();
 
         for (int i = 0; i < BENTO_SIZE; i++) {
