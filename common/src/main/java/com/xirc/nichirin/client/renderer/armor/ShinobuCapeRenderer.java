9e977800c479e1b992bd0c0e7bd6f297197da408
XircTwerk <ddanielcohen2010@gmail.com>
2025-08-08 15:30:18 -0700

    armor now works fine

--- common/src/main/java/com/xirc/nichirin/client/renderer/armor/ShinobuCapeRenderer.java
+++ common/src/main/java/com/xirc/nichirin/client/renderer/armor/ShinobuCapeRenderer.java
@@ -3,9 +3,7 @@ package com.xirc.nichirin.client.renderer.armor;
 import com.xirc.nichirin.client.model.NichirinArmorModel;
 import com.xirc.nichirin.common.item.armor.NichirinArmorItem;
 import mod.azure.azurelib.cache.object.GeoBone;
-import mod.azure.azurelib.util.RenderUtils;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.EquipmentSlot;
 import org.jetbrains.annotations.Nullable;
 
@@ -41,34 +39,12 @@ public class ShinobuCapeRenderer extends NichirinArmorRenderer<NichirinArmorItem
     protected void applyBaseTransformations(HumanoidModel<?> baseModel) {
         super.applyBaseTransformations(baseModel);
 
-        // Handle cape arms scaling and positioning
+        // Stretch cape arms out to the sides
         GeoBone capeLeft = this.model.getBone("capeLeft").orElse(null);
         GeoBone capeRight = this.model.getBone("capeRight").orElse(null);
-        GeoBone cape = this.model.getBone("Cape").orElse(null);
 
-        // Scale cape arms slightly larger as outer layer
-        if (capeLeft != null) capeLeft.setScaleX(1.05f);
-        if (capeRight != null) capeRight.setScaleX(1.05f);
-
-        // Make sure cape body follows player body
-        if (cape != null) {
-            ModelPart bodyPart = baseModel.body;
-            RenderUtils.matchModelPartRot(bodyPart, cape);
-            cape.updatePosition(bodyPart.x, -bodyPart.y, bodyPart.z - 0.1f); // Slightly forward
-        }
-
-        // Position cape arms slightly outward to prevent clipping
-        if (capeLeft != null) {
-            ModelPart leftArmPart = baseModel.leftArm;
-            RenderUtils.matchModelPartRot(leftArmPart, capeLeft);
-            capeLeft.updatePosition(leftArmPart.x - 5.2f, 2f - leftArmPart.y, leftArmPart.z - 0.1f);
-        }
-
-        if (capeRight != null) {
-            ModelPart rightArmPart = baseModel.rightArm;
-            RenderUtils.matchModelPartRot(rightArmPart, capeRight);
-            capeRight.updatePosition(rightArmPart.x + 5.2f, 2f - rightArmPart.y, rightArmPart.z - 0.1f);
-        }
+        if (capeLeft != null) capeLeft.setScaleX(1.2f);  // Stretch 20% wider
+        if (capeRight != null) capeRight.setScaleX(1.2f); // Stretch 20% wider
     }
 
     @Override
